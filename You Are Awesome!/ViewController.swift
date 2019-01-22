e6c4ff49566fe053882887525577d6c00fab419b
Gu Xinran <guxinran@gus-macbook-pro.bc.edu>
2019-01-22 14:21:41 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Gu Xinran on 1/22/19.
+//  Copyright © 2019 Gu Xinran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
