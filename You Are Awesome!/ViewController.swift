05438582ae3dd4523702e6d0f4cb0bf2798b2253
Gu Xinran <guxinran@gus-macbook-pro.bc.edu>
2019-01-24 15:21:10 -0500

    2.10 loop thru array for messages

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,21 +12,35 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+    var index = 0
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You are Awesome!"
-        let message2 = "You are Great!"
-        let message3 = "You are Amazing"
-        if messageLabel.text == message1{
-        messageLabel.text = message2
-        }else if messageLabel.text == message2{
-            messageLabel.text = message3
+        let messages = ["You are Awesome!",
+                        "You are Great!",
+                        "You are fantastic!",
+                        "When the Genius Bar needs help, they call you!",
+                        "You brighten me up!",
+                        "You are da bomb!",
+                        "You are tremdous"]
+        messageLabel.text = messages[index]
+        if index == messages.count - 1 {
+            index = 0
         }else{
-            messageLabel.text = message1
+            index = index + 1
         }
+//        let message1 = "You are Awesome!"
+//        let message2 = "You are Great!"
+//        let message3 = "You are Amazing"
+//        if messageLabel.text == message1{
+//        messageLabel.text = message2
+//        }else if messageLabel.text == message2{
+//            messageLabel.text = message3
+//        }else{
+//            messageLabel.text = message1
+//        }
     }
 
 }
